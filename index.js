bb53c394d77d87bb1d347a0ca8cdccfe41df9c03
sheikhazeem6666-sheikh <sheikhazeem6666@gmail.com>
2026-07-19 06:36:32 +0500

    Add haiku integration to WhatsApp order bot

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const axios = require("axios");
 const admin = require("firebase-admin");
+const { getHaikuReply } = require('./haiku-integration');
 
 const app = express();
 app.use(express.json());
